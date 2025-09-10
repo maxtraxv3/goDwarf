e4a5e86c6fac065242eb77ce5d61da811c1b0854
Carl Otto <33408758+Distortions81@users.noreply.github.com>
2025-09-10 07:48:24 -0600

    fix lanczos upscale uniforms

--- game.go
+++ game.go
@@ -1370,10 +1370,9 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		geo := ebiten.GeoM{}
 		geo.Scale(sx, sy)
 		geo.Translate(tx, ty)
-		nnScale := float32(gs.GameScale)
 		unis := map[string]any{
-			"SrcSize":    [2]float32{float32(offW) / nnScale, float32(offH) / nnScale},
-			"SampleStep": [2]float32{nnScale / float32(offW), nnScale / float32(offH)},
+			"SrcSize":    [2]float32{float32(offW), float32(offH)},
+			"SampleStep": [2]float32{1 / float32(offW), 1 / float32(offH)},
 		}
 		sop := ebiten.DrawRectShaderOptions{Uniforms: unis, Blend: ebiten.BlendCopy}
 		sop.Images[0] = worldView
