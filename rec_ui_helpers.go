252f0c9a291335a6893c12b44a29d0a5beabcea1
Distortions81 <carlotto81@gmail.com>
2025-09-11 12:56:33 -0600

    fix nil pointer issue

--- rec_ui_helpers.go
+++ rec_ui_helpers.go
@@ -7,6 +7,9 @@ var recordingMovie bool
 // updateRecordButton updates the toolbar record button label and theme based on
 // whether we're recording, armed to record, or playing back a movie.
 func updateRecordButton() {
+	if recordBtn == nil {
+		return
+	}
 	if playingMovie || recordingMovie {
 		recordBtn.Text = "STOP"
 	} else {
